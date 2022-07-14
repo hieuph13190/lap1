ccabc931c72f9e2f2e66d921e2553ebe914e5798
hieutran <hieutdph13190@fpt.edu.vn>
2022-07-14 23:26:14 +0700

    init project

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import Navigo from 'navigo'
+import Detail from './pages/detail'
+import Home from './pages/home'
+
+const router = new Navigo('/')
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+interface componentBase {
+  render: () => any
+}
+const print = async (component: componentBase, id) => {
+  app.innerHTML = await component.render(id)
+}
+
+router.on({
+  '/': () => print(Home),
+  '/pokemons/:id': (data) => {
+    const id = data.data.id
+    print(Detail, id)
+  }
+})
+
+router.resolve()
+
+
